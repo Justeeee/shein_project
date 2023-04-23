554af91e0ea123f4caea0af73c21297aa98814de
Justeee <justeee03@gmail.com>
2023-04-23 16:13:42 +0500

    db

--- root/settings.py
+++ root/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'shein',
         'USER': 'postgres',
         'PASSWORD': '0',
         'HOST': 'localhost'
